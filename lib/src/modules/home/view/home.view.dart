2a4deff04176038c810bb928c5ca68f2c135545e
Mhossain028 <meraj.hossain028@gmail.com>
2023-01-21 22:50:51 +0600

    remove unnecessary library

--- lib/src/modules/home/view/home.view.dart
+++ lib/src/modules/home/view/home.view.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../constants/text.dart';
